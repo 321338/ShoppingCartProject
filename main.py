5f3340fe1c5ca3e8615ebac6118dc2ff35959aa3
Alextrolley <397699@nelsongroup.ac.uk>
2024-05-22 17:36:31 +0000

    commit5

--- main.py
+++ main.py
@@ -43,3 +43,75 @@ while True:
 
 # View the final basket
 view_basket(basket)
+
+
+
+
+#Alex Code
+
+print ("These are the avalible items and prices\n")
+
+#  Item code   Item    Price
+#  001         Pizza   £10.00
+#  002         Chips   £5.00
+#  003         Drink   £2.00
+
+#display list
+print ("Code   Product    Price\n")
+
+#directory of availible products, price in pence and number availible
+productDictionary = {
+  "001":["Pizza", "1000","20"],
+  "002":["Chips","500","20"],
+  "003":["Drink","200","100"]
+}
+#print out list of products and price
+
+for x in productDictionary:
+  #tempList is copy of list key x
+  tempList = productDictionary[x]
+  #tempPrice is price of relevant product/100 to display in £
+  tempPrice = int(tempList[1])/100
+  print(x,"  ", tempList[0],"    £",tempPrice)
+print()
+#take order
+#cart total {quantity, total, running total}
+cartTemp={
+  "001":["0","0","0"],
+  "002":["0","0","0"],
+  "003":["0","0","0"]
+}
+total = 0
+runTotal = 0
+quantity = 0
+
+for x in productDictionary:
+  tempString = productDictionary[x]
+  price = int(tempString[1])/100
+  maxQuantity = int(tempString[2])
+  print ("\nWould you like",tempString[0],"? enter y/n")
+  tempAnswer = input()
+  if tempAnswer != "y" and tempAnswer != "n":
+    print ("ERROR")
+    break 
+  elif tempAnswer == "y":
+    print("How many",tempString[0], "would you like?")
+    quantity = int(input())
+    if quantity > maxQuantity :
+      print ("Sorry we can only supply a maximum of ",maxQuantity)
+      quantity = maxQuantity
+  total = price * quantity     
+  runTotal=runTotal+total
+  print (quantity, "x", tempString[0],"= £",total,"     [£",runTotal,"]") 
+  cartTemp[x] = [quantity,total,runTotal]
+
+
+#print order
+print ("\nYour order is for :\n")
+for x in productDictionary:
+  numString = cartTemp[x]
+  type = productDictionary[x]
+  print (numString[0]," x ", type[0], " =  £",numString[1])
+print ("\n      Total  =  £", runTotal)
+print ("\n\n\n        Thank you") 
+print("\n     Please come again")
